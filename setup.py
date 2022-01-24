7c5685e6bebf86fd5e9983f99b9db45957c2217d
Alex Bridge <alex.bridge@torchbox.com>
2022-01-24 16:40:42 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="wagtail-purge",
-    version=0.1,
+    version="0.1.1",
     description="Purge individual URLs from the Wagtail admin",
     long_description=long_description,
     long_description_content_type="text/markdown",
